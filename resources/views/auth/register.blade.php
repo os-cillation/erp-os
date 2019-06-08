e8b34c75a573981c559425352e01437540ec1263
Slatyo <daniel.henze@outlook.com>
2019-06-09 01:03:02 +0200

    style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,69 +9,69 @@
                 </h3>
             </div>
             <div class="uk-card-body">
-            <form method="POST" action="{{ route('register') }}" class="uk-form-stacked">
-                <fieldset class="uk-fieldset">
-                    @csrf
+                <form method="POST" action="{{ route('register') }}" class="uk-form-stacked">
+                    <fieldset class="uk-fieldset">
+                        @csrf
 
-                    <label for="name" class="uk-form-label">{{ __('Name') }}</label>
-                    <div class="uk-form-controls">
-                        <input id="name" type="text"
-                               class="uk-input uk-form-width-large @error('name') uk-form-danger @enderror"
-                               name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <label for="name" class="uk-form-label">{{ __('Name') }}</label>
+                        <div class="uk-form-controls">
+                            <input id="name" type="text"
+                                   class="uk-input uk-form-width-large @error('name') uk-form-danger @enderror"
+                                   name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                    </div>
+                        </div>
 
-                    @error('name')
-                    <span class="invalid-feedback" role="alert">
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                         </span>
-                    @enderror
+                        @enderror
 
 
-                    <label for="email"
-                           class="uk-form-label">{{ __('E-Mail Address') }}</label>
-                    <div class="uk-form-controls">
-                        <input id="email" type="email"
-                               class="uk-input uk-form-width-large @error('email') uk-form-danger @enderror"
-                               name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <label for="email"
+                               class="uk-form-label">{{ __('E-Mail Address') }}</label>
+                        <div class="uk-form-controls">
+                            <input id="email" type="email"
+                                   class="uk-input uk-form-width-large @error('email') uk-form-danger @enderror"
+                                   name="email" value="{{ old('email') }}" required autocomplete="email">
 
-                    </div>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
+                        </div>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                    @enderror
+                        @enderror
 
-                    <label for="password"
-                           class="uk-form-label">{{ __('Password') }}</label>
+                        <label for="password"
+                               class="uk-form-label">{{ __('Password') }}</label>
 
-                    <div class="uk-form-controls">
-                        <input id="password" type="password"
-                               class="uk-input uk-form-width-large @error('password') uk-form-danger @enderror"
-                               name="password"
-                               required autocomplete="new-password">
+                        <div class="uk-form-controls">
+                            <input id="password" type="password"
+                                   class="uk-input uk-form-width-large @error('password') uk-form-danger @enderror"
+                                   name="password"
+                                   required autocomplete="new-password">
 
-                    </div>
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
+                        </div>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                    @enderror
-                    <label for="password-confirm"
-                           class="uk-form-label">{{ __('Confirm Password') }}</label>
+                        @enderror
+                        <label for="password-confirm"
+                               class="uk-form-label">{{ __('Confirm Password') }}</label>
 
-                    <div class="uk-form-controls">
-                        <input id="password-confirm" type="password" class="uk-input uk-form-width-large"
-                               name="password_confirmation" required autocomplete="new-password">
-                    </div>
+                        <div class="uk-form-controls">
+                            <input id="password-confirm" type="password" class="uk-input uk-form-width-large"
+                                   name="password_confirmation" required autocomplete="new-password">
+                        </div>
 
-                    <div class="uk-margin">
-                        <button type="submit" class="uk-button uk-button-primary">
-                            {{ __('Register') }}
-                        </button>
-                    </div>
-                </fieldset>
-            </form>
+                        <div class="uk-margin">
+                            <button type="submit" class="uk-button uk-button-primary">
+                                {{ __('Register') }}
+                            </button>
+                        </div>
+                    </fieldset>
+                </form>
             </div>
             <div class="uk-card-footer">
 
